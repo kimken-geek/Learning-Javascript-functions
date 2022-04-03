ed8e0d3d43bce140e14fe4d8e6cfa2a153abdc91
kimken-geek <kennethkimken@gmail.com>
2022-04-03 21:59:23 +0300

    javascript functions

--- functions/main.js
+++ functions/main.js
@@ -36,7 +36,7 @@ function printFullName () {
     let fullName = firstName + space + middleName + space + lastName
     return fullName
 }
-console.log(printFullName())
+console.log(printFullName()) //returns Kenneth Muriuki Kimathi 
 
 function addTwoNumbers () {
     let a = 20
@@ -44,14 +44,14 @@ function addTwoNumbers () {
     let sum = a + b
     return sum 
 }
-console.log (addTwoNumbers())
+console.log (addTwoNumbers()) //returns 65 
 
 function square1 () {
     let num = 4
     let square = num * num 
     return square 
 }
-console.log (square1())
+console.log (square1()) //returns 16 
 
 //Function with a parameter 
 
@@ -59,10 +59,27 @@ function areaOfCircle (r){
     let area = Math.PI * r * r
     return area
 }
-console.log (areaOfCircle(2))
+console.log (areaOfCircle(2)) //returns 12.5663...
 
 function sumOfTwo(a, b){
     let sum = a+b 
     return sum 
 }
-console.log (sumOfTwo(12, 18))
+console.log (sumOfTwo(12, 18)) //returns 30 
+
+function printFullName (firstName, middleName, lastName) {
+    return `${firstName} ${middleName} ${lastName}`
+}
+console.log(printFullName('Kenneth', 'Muriuki', 'Kimathi')) //returns Kenneth Muriuki Kimathi
+
+//functions with many parameters 
+
+function sumArrayValues (arr){
+    let sum =0;
+    for(let i = 0; i<arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+const numbers = [1,2,3,4,5]; 
+console.log(sumArrayValues(numbers));
